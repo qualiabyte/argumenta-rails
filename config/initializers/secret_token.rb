c7428d12d4eacbed78a0d09a4275f994e458fff4
Tyler Florez <qualiabyte@gmail.com>
2014-01-02 18:07:42 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArgumentaRails::Application.config.secret_key_base = '802296a44fd370d9971f6986add48b710abf3c4232b44a1c69b23bb2c782ac4eef9e45473db4b7cb440f428f036d9695acde5a0c84baad1852a40677651e0cc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArgumentaRails::Application.config.session_store :cookie_store, key: '_argumenta-rails_session'
