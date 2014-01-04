eb2fc842282f11e84d2333d09dee8076e627e785
Tyler Florez <qualiabyte@gmail.com>
2014-01-03 16:08:24 -0800

    Add model tests for proposition

--- test/models/proposition_test.rb
+++ test/models/proposition_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PropositionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :propositions
+
+  def setup
+    @valid = propositions :valid
+    @defaults = @valid.attributes
+  end
+
+  test "validation fails for empty attributes" do
+    proposition = Proposition.new
+    assert proposition.invalid?
+  end
+
+  test "validation passes for valid attributes" do
+    proposition = Proposition.new @defaults
+    assert proposition.valid?
+  end
 end
