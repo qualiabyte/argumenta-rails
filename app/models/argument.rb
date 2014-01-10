c9eca284fb4f5c7e4c5328df406c9a26a2de79a8
Tyler Florez <qualiabyte@gmail.com>
2014-01-09 21:17:13 -0800

    Group validations by attribute

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -1,21 +1,16 @@
 class Argument < ActiveRecord::Base
-
   has_and_belongs_to_many :propositions,
     foreign_key: "argument_sha1",
     association_foreign_key: "proposition_sha1"
 
-  validates :title, :sha1,
-    presence: true
-
-  validates :sha1,
-    uniqueness: true
-
   validates :title,
+    presence: true,
     length: { in: 1..100 }
 
   validates :sha1,
+    presence: true,
+    uniqueness: true,
     format: {
       with: /\A[0-9a-f]{40}\z/,
       message: "must be a 40 character hexadecimal string" }
-
 end
--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -1,18 +1,15 @@
 class Proposition < ActiveRecord::Base
-
   has_and_belongs_to_many :arguments,
     foreign_key: "proposition_sha1",
     association_foreign_key: "argument_sha1"
 
-  validates :text, :sha1,
-    presence: true
-
   validates :text,
+    presence: true,
     length: { in: 1..240 }
 
   validates :sha1,
+    presence: true,
     format: {
       with: /\A[0-9a-f]{40}\z/,
       message: "must be a 40 character hexadecimal string" }
-
 end
