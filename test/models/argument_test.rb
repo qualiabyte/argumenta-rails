39e6c9343103a558fd36eb0fb0df7546c62c6ae3
Tyler Florez <qualiabyte@gmail.com>
2014-01-12 02:42:15 -0800

    Test adding a proposition

--- test/models/argument_test.rb
+++ test/models/argument_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class ArgumentTest < ActiveSupport::TestCase
-  fixtures :arguments
+  fixtures :arguments, :propositions
 
   def setup
-    @valid = arguments :valid
-    @defaults = @valid.attributes
-    @valid.destroy
+    @argument = arguments :valid
+    @defaults = @argument.attributes
+    @proposition = propositions :valid
   end
 
   test "validation fails for empty attributes" do
+    @argument.destroy
     argument = Argument.new
     assert argument.invalid?
   end
 
   test "validation passes for valid attributes" do
+    @argument.destroy
     argument = Argument.new @defaults
     assert argument.valid?, argument.errors.to_hash
+  end
+
+  test "add a proposition" do
+    @argument.propositions.clear
+    @argument.propositions << @proposition
+    assert_equal @proposition, @argument.propositions.first
+    assert_includes @proposition.arguments, @argument
   end
 end
